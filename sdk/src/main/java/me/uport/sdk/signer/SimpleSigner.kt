6fa1095308bd54e7a447f4d083472a913087c01e
Mircea Nistor <mirceanis@gmail.com>
2018-07-12 15:45:32 +0300

    bump several dependency versions and adapt SimpleSigner.kt to changed APIs

--- sdk/src/main/java/me/uport/sdk/signer/SimpleSigner.kt
+++ sdk/src/main/java/me/uport/sdk/signer/SimpleSigner.kt
@@ -2,23 +2,23 @@ package me.uport.sdk.signer
 
 import me.uport.sdk.core.Signer
 import org.kethereum.crypto.ECKeyPair
-import org.kethereum.crypto.Keys
+import org.kethereum.crypto.getAddress
+import org.kethereum.crypto.signMessage
 import org.kethereum.extensions.hexToBigInteger
 import org.kethereum.model.SignatureData
 
-@Suppress("LiftReturnOrAssignment")
 class SimpleSigner(private val privateKey: String) : Signer {
 
-    override fun getAddress() = Keys.getAddress(ECKeyPair.create(privateKey.hexToBigInteger()))
+    override fun getAddress() = ECKeyPair.create(privateKey.hexToBigInteger()).getAddress()
 
     override fun signMessage(rawMessage: ByteArray, callback: (err: Exception?, sigData: SignatureData) -> Unit) {
 
         try {
-            val keyPair = ECKeyPair.Companion.create(privateKey.hexToBigInteger())
-            val sigData = org.kethereum.crypto.signMessage(rawMessage, keyPair)
-            return callback(null, sigData)
+            val keyPair = ECKeyPair.create(privateKey.hexToBigInteger())
+            val sigData = keyPair.signMessage(rawMessage)
+            callback(null, sigData)
         } catch (ex: Exception) {
-            return callback(ex, SignatureData())
+            callback(ex, SignatureData())
         }
 
     }
