253e198d0a38de5de6dfcf9192fedf906d303aa3
Mircea Nistor <mirceanis@gmail.com>
2018-07-26 15:56:33 +0300

    simplify KPAccountCreator using coroutine

--- identity/src/main/java/me/uport/sdk/identity/KPAccountCreator.kt
+++ identity/src/main/java/me/uport/sdk/identity/KPAccountCreator.kt
@@ -3,27 +3,22 @@ package me.uport.sdk.identity
 import android.content.Context
 import com.uport.sdk.signer.UportHDSigner
 import com.uport.sdk.signer.encryption.KeyProtection
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 
-class KPAccountCreator(private val context: Context) : AccountCreator {
+class KPAccountCreator(private val appContext: Context) : AccountCreator {
 
     override fun createAccount(networkId: String, forceRestart: Boolean, callback: AccountCreatorCallback) {
-
-        val signer = UportHDSigner()
-
-        signer.createHDSeed(context, KeyProtection.Level.SIMPLE) { err, rootAddress, _ ->
-            if (err != null) {
-                return@createHDSeed callback(err, Account.blank)
-            }
-            signer.computeAddressForPath(context,
-                    rootAddress,
-                    Account.GENERIC_DEVICE_KEY_DERIVATION_PATH,
-                    "") { ex, deviceAddress, _ ->
-                if (ex != null) {
-                    return@computeAddressForPath callback(err, Account.blank)
-                }
-
-                val acc = Account(
-                        rootAddress,
+        launch {
+            val signer = UportHDSigner()
+            try {
+                val (handle, _) = signer.createHDSeed(appContext, KeyProtection.Level.SIMPLE)
+                val (deviceAddress, _) = signer.computeAddressForPath(appContext,
+                        handle,
+                        Account.GENERIC_DEVICE_KEY_DERIVATION_PATH,
+                        "")
+                val account = Account(
+                        handle,
                         deviceAddress,
                         networkId,
                         deviceAddress,
@@ -33,8 +28,11 @@ class KPAccountCreator(private val context: Context) : AccountCreator {
                         SignerType.KeyPair
                 )
 
-                return@computeAddressForPath callback(null, acc)
+                launch(UI) { callback(null, account) }
+            } catch (err: Exception) {
+                launch(UI) { callback(err, Account.blank) }
             }
+
         }
     }
 
