6189b33a68f32c5fbe2099a95f8643aaac742171
Mircea Nistor <mirceanis@gmail.com>
2018-07-26 17:40:59 +0300

    add a test for KeyPair account imports

--- identity/src/androidTest/java/KPAccountCreatorTest.kt
+++ identity/src/androidTest/java/KPAccountCreatorTest.kt
@@ -5,13 +5,12 @@ import android.support.test.InstrumentationRegistry
 import kotlinx.coroutines.experimental.runBlocking
 import me.uport.sdk.core.Networks
 import org.junit.Assert.*
-import org.junit.Test
-
 import org.junit.Before
+import org.junit.Test
 
 class KPAccountCreatorTest {
 
-    lateinit var appContext: Context
+    private lateinit var appContext: Context
 
     @Before
     fun run_before_every_test() {
@@ -28,6 +27,23 @@ class KPAccountCreatorTest {
             assertTrue(account.address.isNotEmpty())
             assertTrue(account.publicAddress.isNotEmpty())
             assertTrue(account.deviceAddress.isNotEmpty())
+        }
+    }
+
+    @Test
+    fun importAccount() {
+
+        val referenceSeedPhrase = "vessel ladder alter error federal sibling chat ability sun glass valve picture"
+
+        runBlocking {
+            val account = KPAccountCreator(appContext).importAccount(Networks.rinkeby.network_id, referenceSeedPhrase)
+            assertNotNull(account)
+            assertNotEquals(Account.blank, account)
+            assertTrue(account.signerType == SignerType.KeyPair)
+            assertEquals("2opxPamUQoLarQHAoVDKo2nDNmfQLNCZif4", account.address)
+            assertEquals("0x847e5e3e8b2961c2225cb4a2f719d5409c7488c6", account.publicAddress)
+            assertEquals("0x847e5e3e8b2961c2225cb4a2f719d5409c7488c6", account.deviceAddress)
+            assertEquals("0x794adde0672914159c1b77dd06d047904fe96ac8", account.handle)
         }
     }
 }
