414895c4506ceda984aad8b97713b09e4be9f727
Mircea Nistor <mirceanis@gmail.com>
2018-08-07 18:12:19 +0300

    add some extensions

--- core/src/main/java/me/uport/sdk/core/Extensions.kt
+++ core/src/main/java/me/uport/sdk/core/Extensions.kt
@@ -1,6 +1,10 @@
 package me.uport.sdk.core
 
 import android.util.Base64
+import org.kethereum.extensions.toHexStringNoPrefix
+import org.walleth.khex.clean0xPrefix
+import org.walleth.khex.prepend0xPrefix
+import java.math.BigInteger
 
 fun String.toBase64() = Base64.encodeToString(this.toByteArray(), Base64.NO_WRAP)
 fun ByteArray.toBase64() = Base64.encodeToString(this, Base64.NO_WRAP)
@@ -13,3 +17,7 @@ fun String.decodeBase64(): ByteArray = this
         .replace('/', '_')
         .replace("=", "")
         .let { Base64.decode(it.toByteArray(), Base64.URL_SAFE or Base64.NO_PADDING) }
+
+
+fun String.toBytes32String() = clean0xPrefix().padStart(64, '0').prepend0xPrefix()
+fun BigInteger.toBytes32String() = toHexStringNoPrefix().padStart(64, '0').prepend0xPrefix()
