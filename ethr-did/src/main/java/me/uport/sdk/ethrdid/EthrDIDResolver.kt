ba5a6eb2524a52df997b819d529db4531e0c1dc0
Mircea Nistor <mirceanis@gmail.com>
2018-08-16 20:23:27 +0300

    add history parser

--- ethr-did/src/main/java/me/uport/sdk/ethrdid/EthrDIDResolver.kt
+++ ethr-did/src/main/java/me/uport/sdk/ethrdid/EthrDIDResolver.kt
@@ -1,34 +1,84 @@
 package me.uport.sdk.ethrdid
 
+import me.uport.sdk.core.bytes32ToString
+import me.uport.sdk.core.hexToBytes32
+import me.uport.sdk.core.toBase64
+import me.uport.sdk.core.utf8
+import me.uport.sdk.ethrdid.DelegateType.Secp256k1SignatureAuthentication2018
+import me.uport.sdk.ethrdid.DelegateType.Secp256k1VerificationKey2018
+import me.uport.sdk.ethrdid.EthereumDIDRegistry.Events.DIDAttributeChanged
+import me.uport.sdk.ethrdid.EthereumDIDRegistry.Events.DIDDelegateChanged
 import me.uport.sdk.jsonrpc.JsonRPC
 import me.uport.sdk.jsonrpc.JsonRpcBaseResponse
 import me.uport.sdk.jsonrpc.experimental.ethCall
+import me.uport.sdk.jsonrpc.experimental.getLogs
+import org.intellij.lang.annotations.Language
+import org.kethereum.encodings.encodeToBase58String
 import org.kethereum.extensions.hexToBigInteger
+import org.kethereum.extensions.toHexStringNoPrefix
+import org.walleth.khex.hexToByteArray
+import org.walleth.khex.prepend0xPrefix
+import org.walleth.khex.toHexString
 import pm.gnosis.model.Solidity
-
+import java.math.BigInteger
+import java.util.*
 
 class EthrDIDResolver(
-        val rpc : JsonRPC
-)
-{
-
-    //TODO: replace hardcoded coordinates with configuration
-    val registryAddress = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b"
+        private val rpc: JsonRPC,
+        //TODO: replace hardcoded coordinates with configuration
+        val registryAddress: String = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b"
+) {
 
 
-    suspend fun lastChanged(identity : String) : String {
+    suspend fun lastChanged(identity: String): String {
         val encodedCall = EthereumDIDRegistry.Changed.encode(Solidity.Address(identity.hexToBigInteger()))
         val jrpcResponse = rpc.ethCall(registryAddress, encodedCall)
-        //TODO: throw jrpc error
-        return JsonRpcBaseResponse.fromJson(jrpcResponse).result.toString()
+        val parsedResponse = JsonRpcBaseResponse.fromJson(jrpcResponse)
+
+        if (parsedResponse.error != null) throw parsedResponse.error?.toException()!!
+
+        return parsedResponse.result.toString()
     }
 
-//    suspend fun changeLog(identity : String) {
-//        var previousChange = lastChanged(identity)
-//        while (previousChange.isNotBlank() && previousChange != "0") {
-//            var logs = JsonRPC(rpcUrl).getLogs(registryAddress, topics, fromBlock, toBlock)
-//        }
-//    }
+    suspend fun getHistory(identity: String): List<Any> {
+        val lastChangedQueue: Queue<BigInteger> = PriorityQueue<BigInteger>()
+        val events = emptyList<Any>().toMutableList()
+        lastChangedQueue.add(lastChanged(identity).hexToBigInteger())
+        do {
+            val lastChange = lastChangedQueue.remove()
+            val logs = rpc.getLogs(registryAddress, listOf(null, identity.hexToBytes32()), lastChange, lastChange)
+            logs.forEach {
+                val topics: List<String> = it.topics
+                val data: String = it.data
+
+                try {
+                    val event = EthereumDIDRegistry.Events.DIDOwnerChanged.decode(topics, data)
+                    lastChangedQueue.add(event.previouschange.value)
+                    events.add(event)
+                } catch (err: Exception) { /*nop*/
+                }
+
+                try {
+                    val event = EthereumDIDRegistry.Events.DIDAttributeChanged.decode(topics, data)
+                    lastChangedQueue.add(event.previouschange.value)
+                    events.add(event)
+                } catch (err: Exception) { /*nop*/
+                }
+
+                try {
+                    val event = EthereumDIDRegistry.Events.DIDDelegateChanged.decode(topics, data)
+                    lastChangedQueue.add(event.previouschange.value)
+                    events.add(event)
+                } catch (err: Exception) { /*nop*/
+                }
+
+            }
+
+
+        } while (lastChange != null && lastChange != BigInteger.ZERO)
+
+        return events
+    }
 
 //    const lastChanged = async (identity) => {
 //        const result = await didReg.changed(identity)
--- ethr-did/src/test/java/me/uport/sdk/ethrdid/EthrDIDResolverTest.kt
+++ ethr-did/src/test/java/me/uport/sdk/ethrdid/EthrDIDResolverTest.kt
@@ -2,12 +2,16 @@ package me.uport.sdk.ethrdid
 
 import kotlinx.coroutines.experimental.runBlocking
 import me.uport.sdk.core.Networks
-import me.uport.sdk.core.toBytes32String
+import me.uport.sdk.core.hexToBytes32
+import me.uport.sdk.core.utf8
+import me.uport.sdk.ethrdid.EthereumDIDRegistry.Events.DIDOwnerChanged
 import me.uport.sdk.jsonrpc.JsonRPC
 import me.uport.sdk.jsonrpc.experimental.getLogs
 import org.junit.Assert.*
 import org.junit.Test
 import org.kethereum.extensions.hexToBigInteger
+import pm.gnosis.model.Solidity
+import pm.gnosis.utils.hexToByteArray
 import java.math.BigInteger
 
 
@@ -37,7 +41,7 @@ class EthrDIDResolverTest {
         val realAddress = "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74"
         val resolver = EthrDIDResolver(rpc)
         val lastChanged = resolver.lastChanged(realAddress).hexToBigInteger()
-        val logResponse = rpc.getLogs(resolver.registryAddress, listOf(null, realAddress.toBytes32String()), lastChanged, lastChanged)
+        val logResponse = rpc.getLogs(resolver.registryAddress, listOf(null, realAddress.hexToBytes32()), lastChanged, lastChanged)
         assertNotNull(logResponse)
         assertTrue(logResponse.isNotEmpty())
     }
@@ -48,16 +52,27 @@ class EthrDIDResolverTest {
         val realAddress = "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74"
         val resolver = EthrDIDResolver(rpc)
         val lastChanged = resolver.lastChanged(realAddress).hexToBigInteger()
-        val logs = rpc.getLogs(resolver.registryAddress, listOf(null, realAddress.toBytes32String()), lastChanged, lastChanged)
+        val logs = rpc.getLogs(resolver.registryAddress, listOf(null, realAddress.hexToBytes32()), lastChanged, lastChanged)
         println(logs)
-//
-//        val element: Map<String, Any?> = ((parsedResponse.result as List<Map<String, Any>>))[0]
-//        val topics: List<String> = element["topics"] as List<String>
-//        val data: String = element["data"] as String
-//        val args: EthereumDIDRegistry.Events.DIDOwnerChanged.Arguments = EthereumDIDRegistry.Events.DIDOwnerChanged.decode(topics, data)
-//        println(args)
-//        val previousBlock = args.previouschange
-//        println(previousBlock)
+
+        val topics: List<String> = logs[0].topics
+        val data: String = logs[0].data
+        val args: DIDOwnerChanged.Arguments = DIDOwnerChanged.decode(topics, data)
+        println(args)
+        val previousBlock = args.previouschange.value
+        println(previousBlock)
+
+    }
+
+    @Test
+    fun `can parse multiple event logs`() = runBlocking {
+        val rpc = JsonRPC(Networks.rinkeby.rpcUrl)
+        val realAddress = "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74"
+        val resolver = EthrDIDResolver(rpc)
+        val events = resolver.getHistory(realAddress)
+        println(events)
+        assertTrue(events.isNotEmpty())
+    }
 
     }
 
