5c309ba7bb1fa2456cd9a93b158091c23e22ec05
Mircea Nistor <mirceanis@gmail.com>
2018-10-02 17:46:16 +0200

    refactor simple signer tests for uniformity

--- signer/src/test/java/com/uport/sdk/signer/SimpleSignerTests.kt
+++ signer/src/test/java/com/uport/sdk/signer/SimpleSignerTests.kt
@@ -12,12 +12,8 @@ import org.walleth.khex.prepend0xPrefix
 
 class SimpleSignerTests {
 
-    private val tested = SimpleSigner("65fc670d9351cb87d1f56702fb56a7832ae2aab3427be944ab8c9f2a0ab87960")
-
     @Test
     fun `sign ETH calls back without error`() {
-
-        val referencePrivateKey = "3686e245890c7f997766b73a21d8e59f6385e1208831af3862574790cbc3d158"
 
         val expectedSignature = SignatureData(
                 r = "809e3b5ef25f4a3b039139e2fb70f70b636eba89c77a3b01e0c71c1a36d84126".hexToBigInteger(),
@@ -26,7 +22,7 @@ class SimpleSignerTests {
 
         val rawTransactionBytes = "f380850ba43b7400832fefd8949e2068cce22de4e1e80f15cb71ef435a20a3b37c880de0b6b3a7640000890abcdef01234567890".hexToByteArray()
 
-        val signer = SimpleSigner(referencePrivateKey)
+        val signer = SimpleSigner("3686e245890c7f997766b73a21d8e59f6385e1208831af3862574790cbc3d158")
 
         signer.signETH(rawTransactionBytes) { err, sigData ->
             Assert.assertNull(err)
@@ -43,7 +39,9 @@ class SimpleSignerTests {
                 s = "e2b85b3c92c9b4f3cf58de46e7997d8efb6e14b2e532d13dfa22ee02f3a43d5d".hexToBigInteger(),
                 v = 28.toByte())
 
-        tested.signJWT(referencePayload) { err, sigData ->
+        val signer = SimpleSigner("65fc670d9351cb87d1f56702fb56a7832ae2aab3427be944ab8c9f2a0ab87960")
+
+        signer.signJWT(referencePayload) { err, sigData ->
             assertNull(err)
             Assert.assertEquals(referenceSignature, sigData)
         }
@@ -51,7 +49,6 @@ class SimpleSignerTests {
 
     @Test
     fun `signETH coroutine passes`() = runBlocking {
-        val referencePrivateKey = "3686e245890c7f997766b73a21d8e59f6385e1208831af3862574790cbc3d158"
 
         val expectedSignature = SignatureData(
                 r = "809e3b5ef25f4a3b039139e2fb70f70b636eba89c77a3b01e0c71c1a36d84126".hexToBigInteger(),
@@ -60,7 +57,7 @@ class SimpleSignerTests {
 
         val rawTransactionBytes = "f380850ba43b7400832fefd8949e2068cce22de4e1e80f15cb71ef435a20a3b37c880de0b6b3a7640000890abcdef01234567890".hexToByteArray()
 
-        val signer = SimpleSigner(referencePrivateKey)
+        val signer = SimpleSigner("3686e245890c7f997766b73a21d8e59f6385e1208831af3862574790cbc3d158")
         val sigData = signer.signETH(rawTransactionBytes)
 
         assertEquals(expectedSignature, sigData)
@@ -75,12 +72,14 @@ class SimpleSignerTests {
                 s = "e2b85b3c92c9b4f3cf58de46e7997d8efb6e14b2e532d13dfa22ee02f3a43d5d".hexToBigInteger(),
                 v = 28.toByte())
 
-        val sigData = tested.signJWT(referencePayload)
+        val signer = SimpleSigner("65fc670d9351cb87d1f56702fb56a7832ae2aab3427be944ab8c9f2a0ab87960")
+        val sigData = signer.signJWT(referencePayload)
         Assert.assertEquals(referenceSignature, sigData)
     }
 
     @Test
     fun `returned address is non null`() {
-        assertEquals("0x794adde0672914159c1b77dd06d047904fe96ac8", tested.getAddress().prepend0xPrefix())
+        val signer = SimpleSigner("65fc670d9351cb87d1f56702fb56a7832ae2aab3427be944ab8c9f2a0ab87960")
+        assertEquals("0x794adde0672914159c1b77dd06d047904fe96ac8", signer.getAddress().prepend0xPrefix())
     }
 }
