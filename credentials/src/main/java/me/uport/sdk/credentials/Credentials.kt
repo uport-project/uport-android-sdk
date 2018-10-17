4ce823f6cd8aa0f546c66593018abba7e4e9ef6e
Mircea Nistor <mirceanis@gmail.com>
2018-10-17 14:04:29 +0300

    add a method to normalize a potential DID string to a known format (ethr-did or uport-did)

--- credentials/src/main/java/me/uport/sdk/credentials/Credentials.kt
+++ credentials/src/main/java/me/uport/sdk/credentials/Credentials.kt
@@ -1,8 +1,13 @@
 package me.uport.sdk.credentials
 
+import android.support.annotation.VisibleForTesting
+import android.support.annotation.VisibleForTesting.PRIVATE
 import com.uport.sdk.signer.Signer
+import me.uport.mnid.MNID
 import me.uport.sdk.core.ITimeProvider
 import me.uport.sdk.core.SystemTimeProvider
+import me.uport.sdk.jwt.JWTTools
+import me.uport.sdk.jwt.model.JwtHeader
 
 /**
  * The [Credentials] class should allow you to create the signed payloads used in uPort including
@@ -21,6 +26,35 @@ class Credentials(
         shareResp,
         verReq,
         ethtx
+    }
+
+    companion object {
+
+        /**
+         * Attempts to normalize a [potentialDID] to a known format.
+         *
+         * This will transform an ethereum address into an ethr-did and an MNID string into a uport-did
+         */
+        @VisibleForTesting(otherwise = PRIVATE)
+        internal fun normalizeKnownDID(potentialDID: String): String {
+
+            //ignore if it's already a did
+            if (potentialDID.matches("^did:(.*)?:.*".toRegex()))
+                return potentialDID
+
+            //match an ethereum address
+            "^(0[xX])*([0-9a-fA-F]{40})".toRegex().find(potentialDID)?.let {
+                val (_, hexDigits) = it.destructured
+                return "did:ethr:0x$hexDigits"
+            }
+
+            //match an MNID
+            if (MNID.isMNID(potentialDID)) {
+                return "did:uport:$potentialDID"
+            }
+
+            return potentialDID
+        }
     }
 
 }
--- credentials/src/test/java/me/uport/sdk/credentials/CredentialsTest.kt
+++ credentials/src/test/java/me/uport/sdk/credentials/CredentialsTest.kt
@@ -1,5 +1,6 @@
 package me.uport.sdk.credentials
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -8,6 +9,35 @@ class CredentialsTest {
     @Test
     fun `setup works`() {
         assertTrue(true)
+    }
+
+    @Test
+    fun `can normalize a known string format to a DID format`() {
+        val transformations = mapOf(
+                //already did
+                "did:example:something something" to "did:example:something something",
+                "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74" to "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74",
+                "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74#keys-1" to "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74#keys-1",
+                "did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX" to "did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX",
+                "did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX#owner" to "did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX#owner",
+
+                //eth addr to ethrdid
+                "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74" to "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74",
+                "0XF3BEAC30c498d9e26865f34fcaa57dbb935b0d74" to "did:ethr:0xF3BEAC30c498d9e26865f34fcaa57dbb935b0d74",
+                "f3beac30c498d9e26865f34fcaa57dbb935b0d74" to "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74",
+
+                //mnid to uport did
+                "2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX" to "did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX",
+                "5A8bRWU3F7j3REx3vkJWxdjQPp4tqmxFPmab1Tr" to "did:uport:5A8bRWU3F7j3REx3vkJWxdjQPp4tqmxFPmab1Tr",
+
+                //unknown is left intact
+                "0x1234" to "0x1234",
+                "2nQtiQG6Cgm1GYTBaaK" to "2nQtiQG6Cgm1GYTBaaK"
+        )
+
+        transformations.forEach {(orig, expected) ->
+            assertEquals(expected, Credentials.normalizeKnownDID(orig))
+        }
     }
 
 }
