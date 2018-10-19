08055571eb4d521d7bb3568e755de0329df7d17a
Mircea Nistor <mirceanis@gmail.com>
2018-10-19 14:31:10 +0300

    add transport module

---
+++ transport/src/main/java/me/uport/sdk/transport/IntentSender.kt
@@ -0,0 +1,8 @@
+package me.uport.sdk.transport
+
+import android.support.annotation.VisibleForTesting
+import android.support.annotation.VisibleForTesting.PRIVATE
+
+class IntentSender {
+
+}
---
+++ transport/src/test/java/me/uport/sdk/transport/IntentSenderTest.kt
@@ -0,0 +1,14 @@
+package me.uport.sdk.transport
+
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert.*
+import org.junit.Test
+
+class IntentSenderTest {
+
+    @Test
+    fun `setup is ok`() = runBlocking {
+        assertTrue(true)
+    }
+
+}
