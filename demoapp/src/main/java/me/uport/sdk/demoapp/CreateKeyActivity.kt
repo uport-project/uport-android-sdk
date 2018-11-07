c640829e6b271a5b5dcf49dfd48d9a5a43f32e6d
Ugo Amanoh <ugoamanoh@gmail.com>
2018-11-07 18:28:21 +0100

    Create and display key properties and errors

--- demoapp/src/main/java/me/uport/sdk/demoapp/CreateKeyActivity.kt
+++ demoapp/src/main/java/me/uport/sdk/demoapp/CreateKeyActivity.kt
@@ -2,12 +2,25 @@ package me.uport.sdk.demoapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.uport.sdk.signer.UportHDSigner
+import com.uport.sdk.signer.encryption.KeyProtection
 import kotlinx.android.synthetic.main.create_key.*
+import me.uport.sdk.core.decodeBase64
+import org.walleth.khex.toHexString
 
 class CreateKeyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_key)
+
+        create_key_btn.setOnClickListener{
+            UportHDSigner().createHDSeed(this, KeyProtection.Level.SIMPLE) {err, rootAddress, pubKey ->
+                if (err == null) {
+                    public_key_details.text = "publicKey: ${pubKey.decodeBase64().toHexString()}"
+                    address_details.text = "address: $rootAddress"
+                } else error_text.text = "error: ${err.toString()}"
+            }
+        }
     }
 }
