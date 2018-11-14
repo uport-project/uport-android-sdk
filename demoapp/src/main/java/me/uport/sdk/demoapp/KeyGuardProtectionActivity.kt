279f21ba92c68dc1425533533780560d3af18d06
Ugo Amanoh <ugoamanoh@gmail.com>
2018-11-14 12:37:34 +0100

    Signing random data using key

--- demoapp/src/main/java/me/uport/sdk/demoapp/KeyGuardProtectionActivity.kt
+++ demoapp/src/main/java/me/uport/sdk/demoapp/KeyGuardProtectionActivity.kt
@@ -8,10 +8,15 @@ import kotlinx.android.synthetic.main.key_protection_activity.*
 import kotlinx.android.synthetic.main.create_import_key.*
 import kotlinx.android.synthetic.main.key_protection_activity.*
 import me.uport.sdk.core.decodeBase64
+import me.uport.sdk.core.padBase64
+import me.uport.sdk.core.toBase64
 import org.walleth.khex.toHexString
+import java.util.*
 
 
 class KeyGuardProtectionActivity : AppCompatActivity() {
+
+    private var address: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +34,23 @@ class KeyGuardProtectionActivity : AppCompatActivity() {
             UportHDSigner().createHDSeed(this, KeyProtection.Level.SINGLE_PROMPT) { err, rootAddress, pubKey ->
                 if (err == null) {
                     key_details.text = "publicKey: ${pubKey.decodeBase64().toHexString()}"
-                    //address_details.text = "address: $rootAddress"
+                    address = rootAddress
                 } else error.text = "error: ${err.toString()}"
+            }
+        }
+
+        sign_btn.setOnClickListener {
+
+            val msgBytes = ByteArray(3139).also { Random().nextBytes(it) }
+
+            val payload = msgBytes.toBase64().padBase64()
+
+            UportHDSigner().signTransaction(this, address, "m/94'/62'/0'/0/0", payload,"${getString(R.string.app_name)} is requesting your approval to sign a string with a newly minted key") { err, sig ->
+                if (err == null) {
+                    signed_string_details.text = "Signed Successfully : $sig"
+                } else {
+                    error.text = "error: ${err.toString()}"
+                }
             }
         }
     }
