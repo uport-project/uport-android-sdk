7a9696624759fa9b05a88e96ff6ec0b99bfbc2ad
Ugo Amanoh <ugoamanoh@gmail.com>
2018-11-23 17:09:01 +0100

    Created a signed JWT with KPSigner

--- demoapp/src/main/java/me/uport/sdk/demoapp/SignJWTKeyPairSignerActivity.kt
+++ demoapp/src/main/java/me/uport/sdk/demoapp/SignJWTKeyPairSignerActivity.kt
@@ -2,10 +2,43 @@ package me.uport.sdk.demoapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.uport.sdk.signer.KPSigner
+import kotlinx.android.synthetic.main.create_import_key.*
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.launch
+import me.uport.sdk.core.UI
+import me.uport.sdk.jwt.JWTTools
 
-class SignJWTKeyPairSignerActivity: AppCompatActivity() {
+class SignJWTKeyPairSignerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_import_key)
 
+        create_key_btn.text = "Sign JWT"
+
+        // create a JWT payload
+        val payload = mapOf<String, Any>(
+                "claims" to mapOf(
+                        "name" to "Steve Austin",
+                        "dob" to "13 Aug 1976"
+                )
+        )
+
+        // create KeyPair signer
+        val signer = KPSigner("0x1234")
+        val issuerDID = "did:ethr:${signer.getAddress()}"
+
+
+        create_key_btn.setOnClickListener {
+
+            // use coroutine function to create signed JWT and display results
+            GlobalScope.launch (UI){
+                val signedJWT = JWTTools().createJWT(payload, issuerDID, signer, 5000)
+
+                public_key_details.text = "Signed JWT Token: ${signedJWT}"
+
+                address_details.text = "Issuer DID: ${issuerDID}"
+            }
+        }
     }
 }
