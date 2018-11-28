6cc2af7c489b8ac966f353c04b5d8924b1ea99f9
Mircea Nistor <mirceanis@gmail.com>
2018-11-28 15:28:55 +0200

    isolate UI code into UI context coroutine

--- demoapp/src/main/java/me/uport/sdk/demoapp/DIDResolverActivity.kt
+++ demoapp/src/main/java/me/uport/sdk/demoapp/DIDResolverActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.did_resolver.*
 import kotlinx.coroutines.experimental.GlobalScope
 import kotlinx.coroutines.experimental.launch
-import kotlinx.serialization.json.JSON
+import kotlinx.coroutines.experimental.withContext
 import me.uport.sdk.core.UI
 import me.uport.sdk.universaldid.DIDDocument
 import me.uport.sdk.universaldid.UniversalDID
@@ -13,7 +13,7 @@ import me.uport.sdk.universaldid.UniversalDID
 class DIDResolverActivity : AppCompatActivity() {
 
     val ethr_did_text = "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74"
-    val uport_did_text = "did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX"
+    val uport_did_text = "did:uport:2ozs2ntCXceKkAQKX4c9xp2zPS8pvkJhVqC"//""did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,22 +23,28 @@ class DIDResolverActivity : AppCompatActivity() {
         uport_did.text = "Ethr DID\n$uport_did_text"
 
         resolve_btn.setOnClickListener {
-            GlobalScope.launch(UI) {
+            GlobalScope.launch {
 
                 // validate the did before resolving
-                var status = UniversalDID.canResolve(ethr_did_text)
-                if (status) {
+                val ethrDIDResultText = if (UniversalDID.canResolve(ethr_did_text)) {
                     val ddo: DIDDocument = UniversalDID.resolve(ethr_did_text)
 
-                    ethr_did_doc.text = "Ethr DID Document\n${ddo}"
-                } else ethr_did_doc.text = "Invalid Ethr DID"
+                    "Ethr DID Document\n$ddo"
+                } else {
+                    "Invalid Ethr DID"
+                }
 
-                status = UniversalDID.canResolve(uport_did_text)
-                if (status) {
+                withContext(UI) { ethr_did_doc.text = ethrDIDResultText }
+
+                val uportDIDResultText = if (UniversalDID.canResolve(uport_did_text)) {
                     val ddo: DIDDocument = UniversalDID.resolve(uport_did_text)
 
-                    uport_did_doc.text = "Uport DID Document\n${ddo}"
-                } else uport_did_doc.text = "Invalid Uport DID"
+                    "Uport DID Document\n$ddo"
+                } else {
+                    "Invalid Uport DID"
+                }
+
+                withContext(UI) { uport_did_doc.text = uportDIDResultText }
             }
         }
     }
