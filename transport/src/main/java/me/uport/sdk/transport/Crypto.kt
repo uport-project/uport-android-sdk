9a047d99bd31171d5d8054eeb1680de7ccf722fd
Mircea Nistor <mirceanis@gmail.com>
2018-12-11 17:29:46 +0200

    add json (de)serialization to EncryptedMessage class

--- transport/src/main/java/me/uport/sdk/transport/Crypto.kt
+++ transport/src/main/java/me/uport/sdk/transport/Crypto.kt
@@ -1,5 +1,8 @@
 package me.uport.sdk.transport
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
 import me.uport.knacl.nacl
 import me.uport.sdk.core.decodeBase64
 import me.uport.sdk.core.toBase64
@@ -14,12 +17,23 @@ object Crypto {
      * This class encapsulates an encrypted message that was produced using
      * https://github.com/uport-project/specs/blob/develop/messages/encryption.md
      */
+    @Serializable
     data class EncryptedMessage(
+            @SerialName("version")
             val version: String = ASYNC_ENC_ALGORITHM,
+            @SerialName("nonce")
             val nonce: String,
+            @SerialName("ephemPublicKey")
             val ephemPublicKey: String,
+            @SerialName("ciphertext")
             val ciphertext: String
-    )
+    ) {
+        fun toJson(): String = JSON.stringify(EncryptedMessage.serializer(), this)
+
+        companion object {
+            fun fromJson(json: String): EncryptedMessage? = JSON.nonstrict.parse(EncryptedMessage.serializer(), json)
+        }
+    }
 
     /**
      * Calculates the publicKey usable for encryption corresponding to the given [secretKey]
--- transport/src/test/java/me/uport/sdk/transport/CryptoTest.kt
+++ transport/src/test/java/me/uport/sdk/transport/CryptoTest.kt
@@ -2,8 +2,7 @@ package me.uport.sdk.transport
 
 import me.uport.sdk.core.padBase64
 import me.uport.sdk.core.toBase64
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
+import org.junit.Assert.*
 import org.junit.Test
 import org.walleth.khex.hexToByteArray
 
@@ -34,13 +33,40 @@ class CryptoTest {
     }
 
     @Test
-    fun `decrypt EIP1098 message`() {
+    fun `can decrypt EIP1098 message`() {
         val c = Crypto.EncryptedMessage(
                 nonce = "1dvWO7uOnBnO7iNDJ9kO9pTasLuKNlej",
                 ephemPublicKey = "FBH1/pAEHOOW14Lu3FWkgV3qOEcuL78Zy+qW1RwzMXQ=",
                 ciphertext = "f8kBcl/NCyf3sybfbwAKk/np2Bzt9lRVkZejr6uh5FgnNlH/ic62DZzy")
         val decryptedMessage = Crypto.decryptMessage(c, "7e5374ec2ef0d91761a6e72fdf8f6ac665519bfdf6da0a2329cf0d804514b816".hexToByteArray())
         assertEquals("My name is Satoshi Buterin", decryptedMessage)
+    }
+
+    @Test
+    fun `can deserialize json string to EncryptedMessage`() {
+        //language=JSON
+        val json = """{"version":"x25519-xsalsa20-poly1305","nonce":"JAX+g+/e3RnnNXHRS4ct5Sb+XdgYoJeY","ephemPublicKey":"JLBIe7eSVyq6egVexeWrlKQyOukSo66G3N0PlimMUyI","ciphertext":"Yr2o6x831YWFZr6KESzSkBqpMv1wYkxPULbVSZi21J+2vywrVeZnDe/U2GW40wzUpLv4HhFgL1kvt+cORrapsqCfSy2L1ltMtkilX06rJ+Q"}"""
+        val enc = Crypto.EncryptedMessage.fromJson(json)
+        assertNotNull(enc)
+        enc!!
+        assertEquals("x25519-xsalsa20-poly1305", enc.version)
+        assertEquals("JAX+g+/e3RnnNXHRS4ct5Sb+XdgYoJeY", enc.nonce)
+        assertEquals("JLBIe7eSVyq6egVexeWrlKQyOukSo66G3N0PlimMUyI", enc.ephemPublicKey)
+        assertEquals("Yr2o6x831YWFZr6KESzSkBqpMv1wYkxPULbVSZi21J+2vywrVeZnDe/U2GW40wzUpLv4HhFgL1kvt+cORrapsqCfSy2L1ltMtkilX06rJ+Q", enc.ciphertext)
+    }
+
+    @Test
+    fun `can serialize EncryptedMessage to json string`() {
+        val input = Crypto.EncryptedMessage(
+                nonce = "1dvWO7uOnBnO7iNDJ9kO9pTasLuKNlej",
+                ephemPublicKey = "FBH1/pAEHOOW14Lu3FWkgV3qOEcuL78Zy+qW1RwzMXQ=",
+                ciphertext = "f8kBcl/NCyf3sybfbwAKk/np2Bzt9lRVkZejr6uh5FgnNlH/ic62DZzy")
+
+        //language=JSON
+        val expected = """{"version":"x25519-xsalsa20-poly1305","nonce":"1dvWO7uOnBnO7iNDJ9kO9pTasLuKNlej","ephemPublicKey":"FBH1/pAEHOOW14Lu3FWkgV3qOEcuL78Zy+qW1RwzMXQ=","ciphertext":"f8kBcl/NCyf3sybfbwAKk/np2Bzt9lRVkZejr6uh5FgnNlH/ic62DZzy"}"""
+
+        val json = input.toJson()
+        assertEquals(expected, json)
     }
 
 }
