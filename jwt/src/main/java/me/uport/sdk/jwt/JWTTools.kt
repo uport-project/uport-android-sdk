4603f0ebb7307f72e46e82374a448b8842d8b89e
Ugo Amanoh <ugoamanoh@gmail.com>
2019-01-07 18:20:54 +0100

    Converted milliseconds from timeprovider to seconds, fixed the resulting breaks in tests

--- jwt/src/main/java/me/uport/sdk/jwt/JWTTools.kt
+++ jwt/src/main/java/me/uport/sdk/jwt/JWTTools.kt
@@ -173,11 +173,11 @@ class JWTTools(
     suspend fun verify(token: String): JwtPayload? {
         val (_, payload, signatureBytes) = decode(token)
 
-        if (payload.iat != null && payload.iat > (timeProvider.now() + TIME_SKEW)) {
+        if (payload.iat != null && payload.iat > (timeProvider.now()/1000 + TIME_SKEW)) {
             throw InvalidJWTException("Jwt not valid yet (issued in the future) iat: ${payload.iat}")
         }
 
-        if (payload.exp != null && payload.exp <= (timeProvider.now() - TIME_SKEW)) {
+        if (payload.exp != null && payload.exp <= (timeProvider.now()/1000 - TIME_SKEW)) {
             throw InvalidJWTException("JWT has expired: exp: ${payload.exp}")
         }
 
