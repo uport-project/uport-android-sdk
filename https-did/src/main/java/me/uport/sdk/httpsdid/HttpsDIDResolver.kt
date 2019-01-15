87643c4026f2b2bb1a5891bebf01c0fe0c3696fd
Mircea Nistor <mirceanis@gmail.com>
2019-01-15 16:15:39 +0200

    morph callback convention into simple coroutine for HttpsDidResolver

--- https-did/src/main/java/me/uport/sdk/httpsdid/HttpsDIDResolver.kt
+++ https-did/src/main/java/me/uport/sdk/httpsdid/HttpsDIDResolver.kt
@@ -1,29 +1,20 @@
 package me.uport.sdk.httpsdid
 
-import me.uport.sdk.core.urlGet
+import me.uport.sdk.core.experimental.urlGet
 import me.uport.sdk.universaldid.DIDResolver
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 
 open class HttpsDIDResolver : DIDResolver {
     override val method: String = "https"
 
-    override suspend fun resolve(did: String): HttpsIdentityDocument = suspendCoroutine { continuation ->
+    override suspend fun resolve(did: String): HttpsIdentityDocument {
         if (canResolve(did)) {
             val (_, domain) = parseDIDString(did)
-            getProfileDocument(domain) { err, ddo ->
-                if (err != null) {
-                    continuation.resumeWithException(err)
-                } else {
-                    continuation.resume(HttpsIdentityDocument.fromJson(ddo)!!)
-                }
-            }
+            val ddo = getProfileDocument(domain)
+            return HttpsIdentityDocument.fromJson(ddo)!!
         } else {
-            continuation.resumeWithException(java.lang.IllegalArgumentException("The DID('$did') cannot be resolved by the uPort DID resolver"))
+            throw IllegalArgumentException("The DID('$did') cannot be resolved by the uPort DID resolver")
         }
     }
-
 
     override fun canResolve(potentialDID: String): Boolean {
         val (method, _) = parseDIDString(potentialDID)
@@ -35,9 +26,9 @@ open class HttpsDIDResolver : DIDResolver {
      * Given a [domain], obtains the JSON encoded DID doc then tries to convert it to a [HttpsIdentityDocument] object
      *
      */
-    open fun getProfileDocument(domain: String, callback: (err: Exception?, ddo: String) -> Unit) {
+    suspend fun getProfileDocument(domain: String): String {
         val url = "https://$domain/.well-known/did.json"
-        urlGet(url, null, callback)//getProfileDocumentSync(domain)
+        return urlGet(url)
     }
 
     companion object {
