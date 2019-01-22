c33a8c2769cf8986363376e720e6058a7263393e
Mircea Nistor <mirceanis@gmail.com>
2019-01-22 22:25:30 +0200

    add extra comments to more public extensions to improve code quality metric

--- core/src/main/java/me/uport/sdk/core/Extensions.kt
+++ core/src/main/java/me/uport/sdk/core/Extensions.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package me.uport.sdk.core
 
 import org.kethereum.extensions.toHexStringNoPrefix
@@ -24,9 +26,20 @@ fun String.toBase64() = this.toByteArray().toBase64()
  */
 fun String.padBase64() = this.padEnd(this.length + (4 - this.length % 4) % 4, '=')
 
+/**
+ * Converts the bytes of this string into a base64 string usable in a URL
+ */
 fun String.toBase64UrlSafe() = this.toBase64().replace('+', '-').replace('/', '_')
+
+/**
+ * Converts this byte array into a base64 string usable in a URL
+ */
 fun ByteArray.toBase64UrlSafe() = this.toBase64().replace('+', '-').replace('/', '_')
 
+/**
+ * Decodes a base64 string into a bytearray.
+ * Supports unpadded and url-safe strings as well.
+ */
 fun String.decodeBase64(): ByteArray = this
         //force non-url safe and add padding so that it can be applied to all b64 formats
         .replace('-', '+')
@@ -40,8 +53,23 @@ fun String.decodeBase64(): ByteArray = this
         }
 
 
+/**
+ * Converts a hex string to another hex string pre-padded with zeroes until it represents at least 32 bytes
+ */
 fun String.hexToBytes32() = clean0xPrefix().padStart(64, '0').prepend0xPrefix()
+
+/**
+ * Converts this BigInteger into a hex string that is represented by at least 32 bytes.
+ * The hex representation will be left-padded with zeroes if needed
+ */
 fun BigInteger.toBytes32String() = toHexStringNoPrefix().padStart(64, '0').prepend0xPrefix()
 
+/**
+ * shorthand to use the utf8 Charset
+ */
 val utf8: Charset = Charset.forName("UTF-8")
+
+/**
+ * interprets the underlying ByteArray as String
+ */
 fun ByteArray.bytes32ToString() = this.toString(utf8)
