674d1fe7485c096a58b30bde0cce2551706b4739
Ugo Amanoh <ugo@graystatic.co>
2019-02-21 16:03:26 +0100

    Created Ethereum Transaction demo activity

---
+++ demoapp/src/main/java/me/uport/sdk/demoapp/request_flows/EthereumTransactionActivity.kt
@@ -0,0 +1,20 @@
+package me.uport.sdk.demoapp.request_flows
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.uport.sdk.demoapp.R
+
+/**
+ *
+ * This activity demonstrates the flow for creating and sending a [Ethereum Transaction Request]
+ *
+ **/
+
+class EthereumTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.request_flow)
+
+    }
+}
