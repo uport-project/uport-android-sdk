0153da220803cdde342d3d8a7d44552b00b94979
Mircea Nistor <mirceanis@gmail.com>
2019-02-21 20:59:12 +0200

    Convert `DelegateType` into a wrapper type instead of an enum class and clean up some JWT verification code
    This allow more flexibility in DID Document formats without crashing from parsing errors

--- universal-did/src/main/java/me/uport/sdk/universaldid/DIDDocument.kt
+++ universal-did/src/main/java/me/uport/sdk/universaldid/DIDDocument.kt
@@ -1,9 +1,13 @@
 package me.uport.sdk.universaldid
 
 import android.support.annotation.Keep
+import kotlinx.serialization.Decoder
+import kotlinx.serialization.Encoder
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Optional
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
 
 
 /**
@@ -62,13 +66,57 @@ data class ServiceEntry(
         val serviceEndpoint: String
 )
 
+/**
+ * This is a wrapper class for PublicKeyType
+ * It is meant to provide a more typesafe way of dealing with these strings.
+ * This will be ported to inline classes when that feature of kotlin stabilizes and works properly with serialization
+ *
+ * see DID Document spec:
+ * https://w3c-ccg.github.io/did-spec/#did-documents
+ */
 @Keep
-enum class DelegateType {
-    Secp256k1VerificationKey2018,
-    Secp256k1SignatureAuthentication2018,
-    Secp256k1SignatureVerificationKey2018,
-    EcdsaPublicKeySecp256k1,
-    Ed25519VerificationKey2018,
-    RsaVerificationKey2018,
-    Curve25519EncryptionPublicKey, // encryption key. Usage described here: https://github.com/uport-project/specs/blob/develop/pki/diddocument.md
+@Serializable
+data class DelegateType(val name: String) {
+
+    @Serializer(forClass = DelegateType::class)
+    companion object : KSerializer<DelegateType> {
+
+        override fun serialize(output: Encoder, obj: DelegateType) =
+                output.encodeString(obj.name)
+
+        override fun deserialize(input: Decoder): DelegateType = DelegateType(input.decodeString())
+
+
+        //////////////////////////////
+        //some known key types
+        //////////////////////////////
+
+        /**
+         * Default JWT signing key type used by uPort DIDs
+         *
+         * see usage in uPort spec: https://github.com/uport-project/specs/blob/develop/pki/diddocument.md
+         */
+        val Secp256k1VerificationKey2018 = DelegateType("Secp256k1VerificationKey2018")
+
+        /**
+         * references a [Secp256k1VerificationKey2018] in a DID document [AuthenticationEntry]
+         */
+        val Secp256k1SignatureAuthentication2018 = DelegateType("Secp256k1SignatureAuthentication2018")
+
+
+        /**
+         * While not directly generated here, it is treated as [Secp256k1VerificationKey2018]
+         */
+        val Secp256k1SignatureVerificationKey2018 = DelegateType("Secp256k1SignatureVerificationKey2018")
+
+        /**
+         * While not directly generated here, it is treated as [Secp256k1VerificationKey2018]
+         */
+        val EcdsaPublicKeySecp256k1 = DelegateType("EcdsaPublicKeySecp256k1")
+
+        /**
+         * encryption key. Usage described here: https://github.com/uport-project/specs/blob/develop/pki/diddocument.md
+         */
+        val Curve25519EncryptionPublicKey = DelegateType("Curve25519EncryptionPublicKey")
+    }
 }
