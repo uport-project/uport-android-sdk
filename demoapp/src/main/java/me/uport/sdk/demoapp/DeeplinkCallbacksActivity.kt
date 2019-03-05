f3d75854253b6f6b3637eb41209d91961941fd31
Mircea Nistor <mirceanis@gmail.com>
2019-03-05 12:28:59 +0200

    reverted old request flows in demoapp and added separate branch for demonstrating deeplink callbacks

---
+++ demoapp/src/main/java/me/uport/sdk/demoapp/DeeplinkCallbacksActivity.kt
@@ -0,0 +1,87 @@
+package me.uport.sdk.demoapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.uport.sdk.signer.KPSigner
+import kotlinx.android.synthetic.main.activity_deeplink_callbacks.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import me.uport.sdk.core.UI
+import me.uport.sdk.credentials.Credentials
+import me.uport.sdk.credentials.SelectiveDisclosureRequestParams
+import me.uport.sdk.jwt.JWTTools
+import me.uport.sdk.transport.ResponseParser
+import me.uport.sdk.transport.Transports
+import me.uport.sdk.transport.UriResponse
+
+/**
+ * This shows how to interact with the uPort app and receive results in [onActivityResult]
+ */
+class DeeplinkCallbacksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deeplink_callbacks)
+
+        // create a signer
+        val signer = KPSigner("0x1234")
+
+        // create a DID
+        val issuerDID = "did:ethr:${signer.getAddress()}"
+
+        // create the request JWT
+        val cred = Credentials(issuerDID, signer)
+        val params = SelectiveDisclosureRequestParams(
+                requested = listOf("name"),
+                callbackUrl = "https://uport-project.github.io/uport-android-sdk/callbacks"
+        )
+
+        request_details.text = """
+            Selective disclosure
+            Requesting `name` (and implicit DID) from uPort app user.
+        """.trimIndent()
+
+        // make the request
+        btn_send_request.setOnClickListener {
+
+            progress.visibility = View.VISIBLE
+
+            GlobalScope.launch {
+                val requestJWT = cred.createDisclosureRequest(params)
+
+                // Send a signed request to uport via Transports
+                @Suppress("LabeledExpression")
+                Transports().sendExpectingResult(this@DeeplinkCallbacksActivity, requestJWT)
+
+                withContext(UI) {
+                    progress.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response: UriResponse? = ResponseParser.parseActivityResult(requestCode, resultCode, data)
+
+        when (response) {
+            is UriResponse.JWTUriResponse -> {
+                val (_, payloadMap, _) = JWTTools().decodeRaw(response.token)
+                response_details.text = """
+                        profile: ${payloadMap["own"]}
+                        uPort app user DID: ${payloadMap["iss"]}
+                    """.trimIndent()
+
+            }
+            is UriResponse.ErrorUriResponse -> {
+                response_details.text = "error: ${response.message}"
+            }
+            null -> {
+                //process your other domain specific responses
+            }
+        }
+    }
+}
--- demoapp/src/main/java/me/uport/sdk/demoapp/request_flows/SelectiveDisclosureActivity.kt
+++ demoapp/src/main/java/me/uport/sdk/demoapp/request_flows/SelectiveDisclosureActivity.kt
@@ -1,6 +1,5 @@
 package me.uport.sdk.demoapp.request_flows
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -13,12 +12,7 @@ import me.uport.sdk.core.UI
 import me.uport.sdk.credentials.Credentials
 import me.uport.sdk.credentials.SelectiveDisclosureRequestParams
 import me.uport.sdk.demoapp.R
-import me.uport.sdk.jwt.JWTTools
-import me.uport.sdk.transport.ResponseParser
 import me.uport.sdk.transport.Transports
-import me.uport.sdk.transport.UriResponse
-import me.uport.sdk.transport.UriResponse.ErrorUriResponse
-import me.uport.sdk.transport.UriResponse.JWTUriResponse
 
 /**
  *
@@ -63,7 +57,7 @@ class SelectiveDisclosureActivity : AppCompatActivity() {
 
                 // Send a valid signed request to uport via Transports
                 @Suppress("LabeledExpression")
-                Transports().sendExpectingResult(this@SelectiveDisclosureActivity, requestJWT)
+                Transports().send(this@SelectiveDisclosureActivity, requestJWT)
 
                 withContext(UI) {
                     progress.visibility = View.GONE
@@ -71,29 +65,5 @@ class SelectiveDisclosureActivity : AppCompatActivity() {
             }
         }
 
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        val response: UriResponse? = ResponseParser.parseActivityResult(requestCode, resultCode, data)
-
-        when (response) {
-            is JWTUriResponse -> {
-                val (_, payloadMap, _) = JWTTools().decodeRaw(response.token)
-                response_details.text = """
-                        profile: ${payloadMap["own"]}
-                        uPort app User DID: ${payloadMap["iss"]}
-                    """.trimIndent()
-
-            }
-            is ErrorUriResponse -> {
-                //TODO: in this case should error responses be interpreted as activity result codes?
-                response_details.text = "error: ${response.message}"
-            }
-            null -> {
-                //process your other domain specific responses
-            }
-        }
     }
 }
