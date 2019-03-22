84f3f4a4c98c2c24e83ae64d4f2581591b3fa852
Ugo Amanoh <ugo@graystatic.co>
2019-03-22 10:57:14 +0100

    wip: Implemented account interface for MetaIdentityAccount

--- sdk/src/main/java/me/uport/sdk/MetaIdentityAccount.kt
+++ sdk/src/main/java/me/uport/sdk/MetaIdentityAccount.kt
@@ -1,17 +1,15 @@
-package me.uport.sdk.identity
+package me.uport.sdk
 
 import android.content.Context
 import android.support.annotation.VisibleForTesting
 import com.uport.sdk.signer.Signer
-import com.uport.sdk.signer.UportHDSigner
-import com.uport.sdk.signer.UportHDSignerImpl
 import kotlinx.serialization.SerialName
-
+import me.uport.sdk.identity.AccountInterface
 
 /**
- * AccountInterface Implementation for [HDAccount] which are backed up by [UportHDSigner]
+ * AccountInterface Implementation for [MetaIdentityAccount] which are backed up by [MetaIdentitySigner]
  */
-class HDAccount(
+class MetaIdentityAccount(
 
         @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
         @SerialName("uportRoot")
@@ -22,15 +20,24 @@ class HDAccount(
 
         override val publicAddress: String,
 
-        override val network: String
+        override val network: String,
+
+        @SerialName("manager")
+        val identityManagerAddress: String,
+
+        @SerialName("txRelay")
+        val txRelayAddress: String,
+
+        @SerialName("fuelToken")
+        val fuelToken: String
 
 ) : AccountInterface {
 
     override fun getDID(): String {
-        return "did:ethr:$publicAddress"
+        return "did:uport:${getMnid()}"
     }
 
     override fun getSigner(context: Context): Signer {
-        return UportHDSignerImpl(context, UportHDSigner(), rootAddress = handle, deviceAddress = deviceAddress)
+        throw Error("Not yet implemented")
     }
 }
