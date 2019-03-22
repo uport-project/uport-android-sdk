14ab5f1a7d938a243c97f6ff8547264741ea347c
Ugo Amanoh <ugo@graystatic.co>
2019-03-22 11:41:12 +0100

    return MetaIdentitySigner from MetaIdentityAccount

--- sdk/src/main/java/me/uport/sdk/MetaIdentityAccount.kt
+++ sdk/src/main/java/me/uport/sdk/MetaIdentityAccount.kt
@@ -3,8 +3,13 @@ package me.uport.sdk
 import android.content.Context
 import android.support.annotation.VisibleForTesting
 import com.uport.sdk.signer.Signer
+import com.uport.sdk.signer.UportHDSigner
+import com.uport.sdk.signer.UportHDSignerImpl
 import kotlinx.serialization.SerialName
+import me.uport.sdk.core.Networks
 import me.uport.sdk.identity.AccountInterface
+import me.uport.sdk.signer.MetaIdentitySigner
+import me.uport.sdk.signer.TxRelaySigner
 
 /**
  * AccountInterface Implementation for [MetaIdentityAccount] which are backed up by [MetaIdentitySigner]
@@ -38,6 +43,8 @@ class MetaIdentityAccount(
     }
 
     override fun getSigner(context: Context): Signer {
-        throw Error("Not yet implemented")
+        val signer = UportHDSignerImpl(context, UportHDSigner(), rootAddress = handle, deviceAddress = deviceAddress)
+        val relaySigner = TxRelaySigner(signer, Networks.get(network))
+        return MetaIdentitySigner(relaySigner, publicAddress, identityManagerAddress)
     }
 }
