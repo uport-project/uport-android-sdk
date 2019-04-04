9c556e4dbb005b39c23ebcbb5ab31f5eb8d749b2
Ugo Amanoh <ugo@graystatic.co>
2019-04-04 18:44:19 +0100

    Fixed Account references in InMemoryAccountStorage

--- sdk/src/test/java/me/uport/sdk/fakes/InMemoryAccountStorage.kt
+++ sdk/src/test/java/me/uport/sdk/fakes/InMemoryAccountStorage.kt
@@ -1,30 +1,30 @@
 package me.uport.sdk.fakes
 
 import me.uport.sdk.AccountStorage
-import me.uport.sdk.identity.HDAccount
+import me.uport.sdk.identity.Account
 
 /**
  * volatile account storage, usable in tests
  */
 class InMemoryAccountStorage : AccountStorage {
 
-    private val accounts = mapOf<String, HDAccount>().toMutableMap()
+    private val accounts = mapOf<String, Account>().toMutableMap()
 
-    override fun upsert(newAcc: HDAccount) {
+    override fun upsert(newAcc: Account) {
         accounts[newAcc.handle] = newAcc
     }
 
-    override fun upsertAll(list: Collection<HDAccount>) {
+    override fun upsertAll(list: Collection<Account>) {
         list.forEach {
             accounts[it.handle] = it
         }
     }
 
-    override fun get(handle: String): HDAccount? = accounts[handle]
+    override fun get(handle: String): Account? = accounts[handle]
 
     override fun delete(handle: String) {
         accounts.remove(handle)
     }
 
-    override fun all(): List<HDAccount> = accounts.values.toList()
+    override fun all(): List<Account> = accounts.values.toList()
 }
