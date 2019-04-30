4d81f04ce759a189284cb21207deb44231229f93
Mircea Nistor <mirceanis@gmail.com>
2019-04-30 17:24:13 +0300

    remove modules that have been exported to independent repos
    see https://github.com/uport-project/kotlin-did-jwt
    and https://github.com/uport-project/kotlin-common

--- jsonrpc/src/main/java/me/uport/sdk/jsonrpc/TransactionNotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package me.uport.sdk.jsonrpc
-
-/**
- * Thrown when a `txHash` is not known by the ETH network
- */
-class TransactionNotFoundException(txHash: String) : RuntimeException("The transaction with hash=$txHash has not been mined yet")
